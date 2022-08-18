b15ea5ed422ea925c6ddea81ae1f0fda289b0bdb
Mamadou Sylla <m27sylla@gmail.com>
2022-08-18 17:10:53 -0400

    Structure React app done

---
+++ frontend_react/src/container/About/About.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { About, Header, Footer, Testimonial, Skills, Work } from "./container";
+import "./About.scss";
+const About = () => {
+	return <div>About</div>;
+};
+
+export default About;
