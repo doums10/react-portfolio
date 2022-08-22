01d2074b734734395043dc3f80a1be956921c2a2
Mamadou Sylla <m27sylla@gmail.com>
2022-08-22 15:40:36 -0400

    filter is displaying, lets continue

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -3,31 +3,31 @@ import "./Work.scss";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
 import AppWrap from "../../wrapper/AppWrap";
-import { urlFor, client } from "../../client"
+import { urlFor, client } from "../../client";
 const Work = () => {
-	const handleWorkFilter = (item) => {
-
-	}
+	const [activeFilter, setActiveFilter] = useState("All");
+	const handleWorkFilter = (item) => {};
 	return (
 		<>
 			<h2 className="head-text">
-				<span>Portfolio</span> 
+				<span>Portfolio</span>
 			</h2>
 			<div className="app__work-filter">
-			{['UI/UX','Javascript', 'React JS', 'Html/Css','All'].map((item, index) => {
-				<div 
-				className=""
-				key={index}
-				onClick={() => handleWorkFilter(item)}
-
-				
-				
-				
-				>
-
-				</div>
-			})}
+				{["UI/UX", "Javascript", "React JS", "Html/Css", "All"].map(
+					(item, index) => (
+						<div
+							className={`app__work-filter-item app__flex p-text ${
+								activeFilter === item ? "item-active" : ""
+							}`}
+							key={index}
+							onClick={() => handleWorkFilter(item)}
+						>
+							{item}
+						</div>
+					)
+				)}
 			</div>
+			
 		</>
 	);
 };
