2cbb8c1e6bce658b0e0c1cbda356e245216d18d7
Mamadou Sylla <m27sylla@gmail.com>
2022-08-22 15:05:55 -0400

    work component started

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Work.scss";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+import AppWrap from "../../wrapper/AppWrap";
+import { urlFor, client } from "../../client"
 const Work = () => {
 	return <div>Work</div>;
 };
