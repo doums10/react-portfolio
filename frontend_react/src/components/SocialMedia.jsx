7bdc95c74a566c01ece7eadc4845119c7cf17a23
Mamadou Sylla <m27sylla@gmail.com>
2022-08-22 13:54:42 -0400

    socialmedia component added and structure done, navigationdots components started

---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BsTwwiter, BsGithub } from "react-icons/bs";
+
+const SocialMedia = () => {
+		return (
+			<div className="app__social">
+				<div>
+					<BsTwwiter />
+				</div>
+				<div>
+					<BsGithub />
+				</div>
+			</div>
+		);
+}
+
+export default SocialMedia
