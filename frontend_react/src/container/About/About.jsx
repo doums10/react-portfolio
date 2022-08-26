c49db700ab15ed27d024503707d4da31e0f524e4
Mamadou Sylla <m27sylla@gmail.com>
2022-08-25 20:34:36 -0400

    some fixes, about image, css description to cancel sanity database

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
-
 import "./About.scss";
 import { urlFor, client } from "../../client";
 import { AppWrap, MotionWrap } from "../../wrapper";
+import { images } from "../../constants";
 
 // static data removed for sanity usage
-// const abouts = [
-// 	{
-// 		title: "Front-End Developer",
-// 		description: "I am a front end developer. ",
-// 		imgUrl: images.front,
-// 	},
-// ];
+const abouts = [
+	{
+		title: "Front-End Developer",
+		description:
+			"👨‍💻 I've been coding for 3 years. I am a front end developer with a passion for building beautiful functional web applications & learning new technologies.",
+			hobbies:"🏋️‍♂️ When I'm not coding, I practice Crossfit, Basketball and Soccer.",
+		imgUrl: images.front,
+	},
+];
 const About = () => {
-	const [abouts, setAbouts] = useState([]);
-	useEffect(() => {
-		const query = '*[_type == "abouts"]';
-		client.fetch(query).then((data) => setAbouts(data));
-	}, []);
+	// const [abouts, setAbouts] = useState([]);
+	// useEffect(() => {
+	// 	const query = '*[_type == "abouts"]';
+	// 	client.fetch(query).then((data) => setAbouts(data));
+	// }, []);
 
 	return (
 		<>
@@ -34,14 +36,17 @@ const About = () => {
 						whileHover={{ scale: 1.1 }}
 						transition={{ duration: 0.6, type: "tween" }}
 						className="app__profile-item"
-						key={about.title + index}
+						key={abouts.title + index}
 					>
-						<img src={urlFor(about.imgUrl)} alt={about.title} />
+						<img src={about.imgUrl} alt={about.title} />
 						<h2 className="bold-text" style={{ marginTop: 20 }}>
 							{about.title}
 						</h2>
 						<p className="p-text" style={{ marginTop: 10 }}>
 							{about.description}
+						</p>
+						<p className="p-text" style={{ marginTop: 10 }}>
+							{about.hobbies}
 						</p>
 					</motion.div>
 				))}
@@ -50,8 +55,8 @@ const About = () => {
 	);
 };
 
-export default AppWrap
-(MotionWrap(About, 'app__about'),
-'about',
-'app__whitebg'
+export default AppWrap(
+	MotionWrap(About, "app__about"),
+	"about",
+	"app__whitebg"
 );
